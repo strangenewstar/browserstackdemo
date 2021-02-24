d835b5c38c4992c7f0d77556bc4bd8dbde1f85bc
jrosen <jrosen@veracode.com>
2021-02-23 21:52:22 -0500

    updating the ThreadLocal to use Browserstack RemoteDriver

--- src/main/java/com/browserstack/demo/selenium/utils/WebDriverThread.java
+++ src/main/java/com/browserstack/demo/selenium/utils/WebDriverThread.java
@@ -1,24 +1,48 @@
 package com.browserstack.demo.selenium.utils;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverThread {
+
+    public static final String AUTOMATE_USERNAME = "jessicarosen2";
+    public static final String AUTOMATE_ACCESS_KEY = "axDJsQEJxQFzgLTSNTcp";
+    public static final String url = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    //https://live.browserstack.com/dashboard#os=Windows&os_version=7&browser=IE&browser_version=8.0&scale_to_fit=true&url=www.google.com&resolution=responsive-mode&speed=1&start=true
+
     private static ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(() -> {
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability("browserName", "Chrome");
+                caps.setCapability("version", "latest");
+                caps.setCapability("platform", "Windows");
+                caps.setCapability("screenResolution", "1024x768");
+                caps.setCapability("name", "Browserstack Demo Test"); // test name
+                caps.setCapability("build", "test"); // CI/CD job or build name
+                try {
+                    return new RemoteWebDriver(new URL(url), caps);
+                } catch (MalformedURLException e) {
+                    return null;
+                }
+            });
 //        Driver.OS_TYPE os = ConfigPropertyUtils.getOSType();
 //        if (Driver.OS_TYPE.MAC.equals(os) || Driver.OS_TYPE.LINUX.equals(os)) {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+//            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 //        } else if (Driver.OS_TYPE.WINDOWS.equals(os)) {
 //            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 //        }
 
-        ChromeOptions options = new ChromeOptions();
-        options.setAcceptInsecureCerts(Boolean.TRUE);
-        options.addArguments("--no-sandbox", "--ignore-certificate-errors");
+//        ChromeOptions options = new ChromeOptions();
+//        options.setAcceptInsecureCerts(Boolean.TRUE);
+//        options.addArguments("--no-sandbox", "--ignore-certificate-errors");
+//
+//        return new ChromeDriver(options); //You can use other driver based on your requirement.
 
-        return new ChromeDriver(options); //You can use other driver based on your requirement.
-    });
+
 
     public static WebDriver getDriver() {
         System.out.println("driver: " + driver);
